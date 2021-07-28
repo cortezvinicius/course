4edc5906d501f32addcb00e1abd410aff8e909e9
cortezvinicius <cortezvinicius881@gmail.com>
2021-07-27 22:27:17 -0300

    Entities

---
+++ src/main/java/com/vcinsidedigital/course/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.vcinsidedigital.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vcinsidedigital.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource 
+{
+	@GetMapping
+	public ResponseEntity<User> findAll()
+	{
+		User u = new User(1L, "Vinicius Cortez", "cortezvinicius881@gmail.com", "9999999", "123456");
+		
+		return ResponseEntity.ok().body(u);
+	}
+}
